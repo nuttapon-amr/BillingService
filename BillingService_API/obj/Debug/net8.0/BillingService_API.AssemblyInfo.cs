1d0da3d8c2c078be4a1b951187a7bdd10a7db74f
nuttapon <nuttapon@amrasia.com>
2026-06-01 15:04:42 +0700

    update assembly informational version and cache files for BillingService_API and DataAccess

--- BillingService_API/obj/Debug/net8.0/BillingService_API.AssemblyInfo.cs
+++ BillingService_API/obj/Debug/net8.0/BillingService_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BillingService_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84f140c3afa8c423b8d0eac39dac2b2ef12f96c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BillingService_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BillingService_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
